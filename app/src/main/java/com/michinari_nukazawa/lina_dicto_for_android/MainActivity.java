757b2123ec01f6e0f6fbbba73757a677393b25b0
Michinari Nukazawa <michinari.nukazawa@gmail.com>
2020-01-18 22:04:10 +0900

    MainActivity cleanup and debug log

--- app/src/main/java/com/michinari_nukazawa/lina_dicto_for_android/MainActivity.java
+++ app/src/main/java/com/michinari_nukazawa/lina_dicto_for_android/MainActivity.java
@@ -4,36 +4,49 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "LinadMainAct";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        WebView webView = (WebView) findViewById(R.id.webView1);
+        super.onCreate(savedInstanceState);
         if (savedInstanceState == null) {
+            Log.d(TAG, "onCreate init");
+
+            setContentView(R.layout.activity_main);
+            WebView webView = (WebView) findViewById(R.id.webView1);
+
             webView.setBackgroundColor(Color.TRANSPARENT);
             webView.loadUrl("file:///android_asset/lina_dicto/index.html");
+
+
+            WebSettings webSettings = webView.getSettings();
+            webSettings.setJavaScriptEnabled(true);
+            webView.getSettings().setAllowFileAccessFromFileURLs(true);
+
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                webView.setWebContentsDebuggingEnabled(true);
+            }
+
         }else{
+            Log.d(TAG, "onCreate restore");
+
+            WebView webView = (WebView) findViewById(R.id.webView1);
             webView.restoreState(savedInstanceState);
-        }
-
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webView.getSettings().setAllowFileAccessFromFileURLs(true);
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            webView.setWebContentsDebuggingEnabled(true);
         }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState )
     {
+        Log.d(TAG, "onSaveInstanceState");
+
         super.onSaveInstanceState(outState);
         WebView webView = (WebView)findViewById(R.id.webView1);
         webView.saveState(outState);
@@ -42,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState)
     {
+        Log.d(TAG, "onRestoreInstanceState");
+
         super.onRestoreInstanceState(savedInstanceState);
         WebView webView = (WebView)findViewById(R.id.webView1);
         webView.restoreState(savedInstanceState);
